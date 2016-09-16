e648c48834be4d64af4d0baa7e97b010b78a44e8
jorissparla <joris.sparla@gmail.com>
2016-09-16 22:58:17 +0200

    feat(random): pass a number to a function
    closes #2

--- src/index.test.js
+++ src/index.test.js
@@ -20,6 +20,14 @@ describe('starwars-names', function() {
         it('Should return a random Starwars character', function() {
             var randomItem = starWars.random();
             expect(starWars.all).to.include(randomItem);
+        });
+        it('Should return an array of random items if passed a number', function(){
+            var randomItems = starWars.random(3);
+            expect(randomItems).to.have.length(3);
+            randomItems.forEach(function(item) {
+                expect(starWars.all).to.include(item);
+            })
         })
+
     })
 })
