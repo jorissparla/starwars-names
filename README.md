8da78ad03eb32e9fcd706fbd04a5b10bbf58a8e1
jorissparla <joris.sparla@gmail.com>
2016-09-17 21:29:58 +0200

    test(babel): babel
    babel
    
    none

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# starwars-names" 
+# starwars-names
+
+[![travis build](https://img.shields.io/travis/jorissparla/starwars-names.svg?maxAge=2592000)](https://travis-ci.org/jorissparla/starwars-names)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-var starWarsNames = require('./starwars-names.json');
-var uniqueRandomArray = require('unique-random-array');
-var getRandomItem = uniqueRandomArray(starWarsNames);
+import starWarsNames from './starwars-names.json';
+import uniqueRandomArray from 'unique-random-array';
+
+const getRandomItem = uniqueRandomArray(starWarsNames);
 module.exports = {
     all: starWarsNames,
     random: random
